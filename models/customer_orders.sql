b072f6dba6eebcb9d34b82af30a54e7d03f580fa
jonathan_sanchez@hakkoda.io <jonathan_sanchez@hakkoda.io>
2024-02-15 20:26:59 +0000

    changed model

--- models/customer_orders.sql
+++ models/customer_orders.sql
@@ -11,15 +11,15 @@ left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, s
         from raw.stripe.payment
         where STATUS <> 'fail'
         group by 1) p ON orders.ID = p.order_id
-left join raw.jaffle_shop.customers C on orders.USER_ID = C.ID ),
+left join DBT_CERT_JSV.DBT_JSANCHEZ.customers C on orders.USER_ID = C.ID ),
 
 customer_orders 
 as (select C.ID as customer_id
     , min(ORDER_DATE) as first_order_date
     , max(ORDER_DATE) as most_recent_order_date
     , count(ORDERS.ID) AS number_of_orders
-from raw.jaffle_shop.customers C 
-left join raw.jaffle_shop.orders as Orders
+from DBT_CERT_JSV.DBT_JSANCHEZ.customers C 
+left join DBT_CERT_JSV.DBT_JSANCHEZ.orders as Orders
 on orders.USER_ID = C.ID 
 group by 1)
 
